e205bfc02bca70cc6115b61c64805b9ed037984f
TealShift <tealshift@pkfitness.com>
2022-04-27 10:51:01 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CardiOp
+A framework to let developers easily connect their iPhones, iPads or Mac to CardiOp app on the Apple Watch to receive realtime Heart Rate
+
+Please contact hello@pkfitness.com for documentation and a license key required to access the SDK
---
+++ Sources/CardiOp.xcframework/ios-arm64/CardiOpFramework.framework/Headers/CardiOpFramework.h
@@ -0,0 +1,18 @@
+//
+//  CardiOpFramework.h
+//  CardiOpFramework
+//
+//  Created by Krishnaprasad Jagadish on 04/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardiOpFramework.
+FOUNDATION_EXPORT double CardiOpFrameworkVersionNumber;
+
+//! Project version string for CardiOpFramework.
+FOUNDATION_EXPORT const unsigned char CardiOpFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardiOpFramework/PublicHeader.h>
+
+
